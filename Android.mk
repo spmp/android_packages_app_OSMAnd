01ae9b66427b5ab082008a325e784c975fde8309
Jasper Aorangi <jasper.mackenzie@gmail.com>
2019-05-26 11:23:10 +1200

    * Replace OUT with OUT_DIR

--- Android.mk
+++ Android.mk
@@ -9,8 +9,8 @@ LOCAL_MODULE := OSMAnd
 
 LOCAL_CERTIFICATE := platform
 
-MY_OUT := $(OUT)/third-party
-LOCAL_SRC_FILES := $(MY_OUT)/android/OsmAnd/outputs/apk/fullLegacyFat/debug/OsmAnd-full-legacy-fat-debug.apk
+MY_OUT_DIR := $(OUT_DIR)/third-party
+LOCAL_SRC_FILES := $(MY_OUT_DIR)/android/OsmAnd/outputs/apk/fullLegacyFat/debug/OsmAnd-full-legacy-fat-debug.apk
 
 LOCAL_MODULE_CLASS := APPS
 LOCAL_OVERRIDES_PACKAGES := Maps
@@ -31,4 +31,4 @@ endif
 # Build defines
 MY_JAVA_OPTIONS := -Xmx40960m
 
-$(info $(shell (_JAVA_OPTIONS=$(MY_JAVA_OPTIONS) ANDROID_HOME="$(MY_TOPDIR)/$(HISTORICAL_SDK_VERSIONS_ROOT)" JAVA_HOME="$(MY_TOPDIR)/$(ANDROID_JAVA8_HOME)" OUT="$(MY_OUT)" GRADLE_USER_HOME="$(GRADLE_USER_HOME)" $(LOCAL_PATH)/./build.sh)))
+$(info $(shell (_JAVA_OPTIONS=$(MY_JAVA_OPTIONS) ANDROID_HOME="$(MY_TOPDIR)/$(HISTORICAL_SDK_VERSIONS_ROOT)" JAVA_HOME="$(MY_TOPDIR)/$(ANDROID_JAVA8_HOME)" OUT_DIR="$(MY_OUT_DIR)" GRADLE_USER_HOME="$(GRADLE_USER_HOME)" $(LOCAL_PATH)/./build.sh)))
