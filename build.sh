c035c64329c6073b9b3a16c28f7a18a3ee4749ed
Jasper Aorangi <jasper.mackenzie@gmail.com>
2019-05-26 13:35:51 +1200

    * Ensure gradle uses no daemon and stops

--- build.sh
+++ build.sh
@@ -4,13 +4,16 @@ cd "${DIR}/android/OsmAnd"
 
 OSM_LOG_FILE="${OSM_LOG_FILE:-/tmp/build_osmand.log}"
 
-../gradlew  --refresh-dependencies clean assembleFullLegacyFatDebug 2>&1 > "${OSM_LOG_FILE}"
+./gradlew --stop
+../gradlew  --refresh-dependencies clean assembleFullLegacyFatDebug &> "${OSM_LOG_FILE}"
 
 if [[ $? -eq 0 ]]
 then
         echo "BUILD SUCCESSFUL!"
+        ./gradlew --stop
         exit 0
 else
         echo "FAIL"
+        ./gradlew --stop
         exit 1
 fi
